50c6483326f970c19bd01196cfffcc5d2eab0a60
SK <sarthakkarandikar03@gmail.com>
2023-03-30 12:41:41 +0000

    Mar 30, 2023, 6:11 PM

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -18,5 +18,7 @@ contract Faucet{
         return funders;
     }
 
-    function getFunder(uint8 index) external view returns ()
+    function getFunder(uint8 index) external view returns (address){
+        
+    }
 }
