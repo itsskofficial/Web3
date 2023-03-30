92f96bc21abce0774be0cd4b98226910d048b515
SK <sarthakkarandikar03@gmail.com>
2023-03-30 11:59:24 +0000

    Mar 30, 2023, 5:29 PM

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.19;
 contract Faucet{
     receive() external payable {}
     function addFunds() external payable {}
-    function name(type name) {
+    function test(type name) {
         
     }
 }
