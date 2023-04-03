efd19077817c252bed8a08a4ef32ac2c7c1754f8
SK <sarthakkarandikar03@gmail.com>
2023-04-03 00:25:14 +0000

    Apr 3, 2023, 5:55 AM

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -30,9 +30,7 @@ contract Faucet{
     }
 
     function withdraw(uint amount) external {
-        require(amount<=1000000000000000000,"Cannot withdraw more than one ether")
-        if (amount<1000000000000000000){
-            payable(msg.sender).transfer(amount);
-        }
+        require(amount<=1000000000000000000,"Cannot withdraw more than one ether");
+        payable(msg.sender).transfer(amount);
     }
 }
