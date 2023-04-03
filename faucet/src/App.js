7c0b1e4697d9a041b1456c37e59e72cc10422457
SK <sarthakkarandikar03@gmail.com>
2023-04-03 07:39:06 +0000

    Apr 3, 2023, 1:09 PM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -22,9 +22,11 @@ function App() {
           console.error("User denied account access")
         }
       }
+
       else if (window.web3){
         provider = window.web3.currentProvider
       }
+        
       else if (!process.env.production){
         provider = new Web3.providers.HttpProvider("http://localhost:7545")
       }
