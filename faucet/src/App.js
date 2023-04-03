d3faddee265678debc0c240d6d392b99f39f64d4
SK <sarthakkarandikar03@gmail.com>
2023-04-03 08:24:07 +0000

    Apr 3, 2023, 1:54 PM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -76,7 +76,7 @@ function App() {
       <div className={classes.wrapper}>
         <div className={classes.faucet}>
           <div className={classes.account}>
-            Account : {account ? account: <}
+            Account : {account ? account: <p>Not Connected</p>}
           </div>
           <div className={classes.balance}>
             Current balance is <strong>10</strong> ETH
