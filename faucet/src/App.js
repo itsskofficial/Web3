ea63b5beae7feb6cbf5734d72d52ed53f1829e41
SK <sarthakkarandikar03@gmail.com>
2023-04-03 06:25:40 +0000

    Apr 3, 2023, 11:55 AM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -4,7 +4,7 @@ import classes from './css/App.module.css'
 function App() {
 
   const loadProvider = async () => {
-    
+    const accounts = await window.ethereum.
   }
   return (
     <Fragment>
