b772bc37c6aba2c8efbd5fa9e54b7d65fd0acd18
SK <sarthakkarandikar03@gmail.com>
2023-04-03 00:24:36 +0000

    Apr 3, 2023, 5:54 AM

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -30,7 +30,7 @@ contract Faucet{
     }
 
     function withdraw(uint amount) external {
-        require()
+        require(amount<=)
         if (amount<1000000000000000000){
             payable(msg.sender).transfer(amount);
         }
