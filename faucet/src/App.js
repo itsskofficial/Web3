7d044e374f6a3281698f6024f086d6e13e3bf725
SK <sarthakkarandikar03@gmail.com>
2023-04-05 05:43:21 +0000

    Apr 5, 2023, 11:13 AM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -22,7 +22,9 @@ function App() {
   
   const changeAccountListener = (provider) => {
     provider.on('accountsChanged', accounts => setAccount(accounts[0]))
-    provider._jsonRpcConnection.events.on('notification', ())
+    provider._jsonRpcConnection.events.on('notification', () => {
+      
+    })
   }
 
   useEffect(() => {
