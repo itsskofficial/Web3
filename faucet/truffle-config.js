ab4f011b6edfba32495a7d6b5570b0b9e63376d2
SK <sarthakkarandikar03@gmail.com>
2023-04-05 00:16:06 +0000

    Apr 5, 2023, 5:46 AM

--- faucet/truffle-config.js
+++ faucet/truffle-config.js
@@ -4,9 +4,9 @@ module.exports = {
   networks: {
    
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     host: "127.0.0.1",   )
+     port: 7545,            
+     network_id: "*",       
     },
     //
     // An additional network, but with some advanced options…
