f6b078d1afacc5bf8488e6e5535cfa85d0cadc81
SK <sarthakkarandikar03@gmail.com>
2023-04-05 00:36:25 +0000

    Apr 5, 2023, 6:06 AM

--- faucet/src/utils/loadContract.js
+++ faucet/src/utils/loadContract.js
@@ -4,7 +4,8 @@ const loadContract = async (name, provider) => {
     const res = await fetch(`/contracts/${name}.json`)
     const Artifact = await res.json()
     const abstraction = contract(Artifact)
-    
+    abstraction.setProvider(provider)
+    const awa
 }
 
 export default loadContract
