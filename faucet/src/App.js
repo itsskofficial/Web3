2bb229f25d9cdf2207a03f60a657faad8cf0e7bb
SK <sarthakkarandikar03@gmail.com>
2023-04-05 05:43:44 +0000

    Apr 5, 2023, 11:13 AM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -23,7 +23,8 @@ function App() {
   const changeAccountListener = (provider) => {
     provider.on('accountsChanged', accounts => setAccount(accounts[0]))
     provider._jsonRpcConnection.events.on('notification', (payload) => {
-
+      const method = { payload }
+      
     })
   }
 
