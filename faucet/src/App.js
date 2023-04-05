a8f01c003aa23e1703a3525c098b9bcd055e8ae4
SK <sarthakkarandikar03@gmail.com>
2023-04-05 07:17:17 +0000

    Apr 5, 2023, 12:47 PM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -99,6 +99,7 @@ function App() {
             Account : {account ? account :
               !web3API.provider ?
                 <div>
+                  
               <button className={classes.wallet} onClick={() => {
                 web3API.provider.request({method:'eth_requestAccounts'})
               }}>
