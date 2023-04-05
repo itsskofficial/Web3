c214c14c14aa221e7eb4f89094efbc1139ad2942
SK <sarthakkarandikar03@gmail.com>
2023-04-05 00:35:42 +0000

    Apr 5, 2023, 6:05 AM

--- faucet/src/utils/loadContract.js
+++ faucet/src/utils/loadContract.js
@@ -1,6 +1,6 @@
 import contract from '@truffle/contract'
 
-const loadContract = async (name) => {
+const loadContract = async (name, provider) => {
     const res = await fetch(`/contracts/${name}.json`)
     const Artifact = await res.json()
     return contract(Artifact)
