c8c4600b456c0a9ae1af3a73421e0df967757a29
SK <sarthakkarandikar03@gmail.com>
2023-04-05 00:30:45 +0000

    Apr 5, 2023, 6:00 AM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -18,7 +18,7 @@ function App() {
     const loadProvider = async () => {
 
       const provider = await detectEthereumProvider()
-      const contract = loadContract(Fa)
+      const contract = loadContract('Faucet')
       if (provider) {
         setWeb3API({
           web3: new Web3(provider),
