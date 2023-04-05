03a4a6379d8935b6ad426e9668521b835c87cbfe
SK <sarthakkarandikar03@gmail.com>
2023-04-05 00:16:45 +0000

    Apr 5, 2023, 5:46 AM

--- faucet/truffle-config.js
+++ faucet/truffle-config.js
@@ -9,36 +9,12 @@ module.exports = {
      network_id: "*",       
     },
   },
-
-  
   mocha: {
   },
-
-  
   compilers: {
     solc: {
       version: "0.8.19",     
     }
   },
 
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: false,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "indexeddb",
-  //     settings: {
-  //       directory: ".db"
-  //     }
-  //   }
-  // }
 };
