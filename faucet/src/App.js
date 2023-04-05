ad7606af2351f7eb2bc42aba1089fd902bccdc8c
SK <sarthakkarandikar03@gmail.com>
2023-04-05 07:43:33 +0000

    Apr 5, 2023, 1:13 PM

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -131,7 +131,7 @@ function App() {
             Current balance is <strong>{balance}</strong> ETH
           </div>
           <div className={classes.actions}>
-            <button className={classes.deposit} onClick={addFunds} disabled={!account && !networkConnected}>
+            <button className={classes.deposit} onClick={addFunds} disabled={!account || !networkConnected}>
               Deposit
             </button>
             <button className={classes.withdraw} onClick={withdraw} disabled={!account}>
