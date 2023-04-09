7955cd9bcdcca277c90f845abc2d3afa69350191
SK <sarthakkarandikar03@gmail.com>
2023-04-09 14:19:53 +0000

    Apr 9, 2023, 7:49 PM

--- course-marketplace/components/layout/base/index.js
+++ course-marketplace/components/layout/base/index.js
@@ -3,14 +3,14 @@ import { Fragment } from "react";
 export default function BaseLayout(props) {
     return (
         <Fragment>
+            <div className="relative max-w-7xl mx-auto px-4">
+            <Navbar />
+                    <div className="fit">
             
+                    </div>
+                </div>
+            <Footer />
         </Fragment>
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            
-          </div>
-        </div>
-        <Footer />
+        
     )
 }
