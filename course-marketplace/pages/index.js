2f882f1b568e413f4d12faab3e0736081ec81bb4
SK <sarthakkarandikar03@gmail.com>
2023-04-09 11:27:41 +0000

    Apr 9, 2023, 4:57 PM

--- course-marketplace/pages/index.js
+++ course-marketplace/pages/index.js
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Fragment } from 'react'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
