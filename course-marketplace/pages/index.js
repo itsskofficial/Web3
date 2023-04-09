92389cff3c98fe2482e8916609191bec68e8abc8
SK <sarthakkarandikar03@gmail.com>
2023-04-09 14:18:28 +0000

    Apr 9, 2023, 7:48 PM

--- course-marketplace/pages/index.js
+++ course-marketplace/pages/index.js
@@ -1,11 +1,14 @@
 import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
 import { CourseList } from "@components/course"
+import BaseLayout from "@components/layout/base"
 import { OrderCard } from "@components/order"
 import { EthRates, WalletBar } from "@components/web3"
 
 export default function Home() {
   return (
+      <BaseLayout>
       
+      </BaseLayout>
         <div className="relative max-w-7xl mx-auto px-4">
           <Navbar />
           <div className="fit">
