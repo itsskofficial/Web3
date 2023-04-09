826864d86fb49e57c075076e3c8c0dd78fe6a384
SK <sarthakkarandikar03@gmail.com>
2023-04-09 14:22:26 +0000

    Apr 9, 2023, 7:52 PM

--- course-marketplace/pages/course.js
+++ course-marketplace/pages/course.js
@@ -8,11 +8,10 @@ import {
 export default function Course() {
 
   return (
-    <div className="relative max-w-7xl mx-auto px-4">
+    
       <CourseHero />
       <Keypoints />
       <Curriculum />
       <Modal />
-    </div>
   )
 }
