5cf442e1bcb9657913e5c208eb31f0d16d6b3a7f
SK <sarthakkarandikar03@gmail.com>
2023-04-09 14:19:17 +0000

    Apr 9, 2023, 7:49 PM

--- course-marketplace/components/layout/base/index.js
+++ course-marketplace/components/layout/base/index.js
@@ -1,5 +1,6 @@
 export default function BaseLayout(props) {
     return (
+        <Frag
         <div className="relative max-w-7xl mx-auto px-4">
           <Navbar />
           <div className="fit">
