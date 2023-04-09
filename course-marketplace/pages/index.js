84ffa5c30f7903d925fc0913d8dd85e2e99e88b0
SK <sarthakkarandikar03@gmail.com>
2023-04-09 14:04:20 +0000

    Apr 9, 2023, 7:34 PM

--- course-marketplace/pages/index.js
+++ course-marketplace/pages/index.js
@@ -1,13 +1,25 @@
-import { Fragment } from 'react'
+import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
+import { CourseList } from "@components/course"
+import { OrderCard } from "@components/order"
+import { EthRates, WalletBar } from "@components/web3"
 
-const Home = () => {
+export default function Home() {
   return (
-    <Fragment>
-      <h1>
-        Hello World!
-      </h1>
-    </Fragment>
+    <div>
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          <Navbar />
+          <div className="fit">
+            <Hero />
+            <Breadcrumbs />
+            <WalletBar />
+            <EthRates />
+            <OrderCard />
+            <CourseList />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
   )
 }
-
-export default Home
