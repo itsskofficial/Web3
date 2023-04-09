6a3ba2d949c17c7c0a589cba9ea713cfb3d7972e
SK <sarthakkarandikar03@gmail.com>
2023-04-09 14:18:03 +0000

    Apr 9, 2023, 7:48 PM

--- course-marketplace/pages/index.js
+++ course-marketplace/pages/index.js
@@ -5,7 +5,7 @@ import { EthRates, WalletBar } from "@components/web3"
 
 export default function Home() {
   return (
-    <div>
+
       <div className="relative bg-white overflow-hidden">
         <div className="relative max-w-7xl mx-auto px-4">
           <Navbar />
