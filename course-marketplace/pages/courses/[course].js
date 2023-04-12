4e3ea9a8e0fb61ab4a75459ea961b5692dd595d4
SK <sarthakkarandikar03@gmail.com>
2023-04-12 15:19:34 +0000

    Apr 12, 2023, 8:49 PM

--- course-marketplace/pages/courses/[course].js
+++ course-marketplace/pages/courses/[course].js
@@ -19,3 +19,28 @@ export default function Course() {
     </BaseLayout>  
   )
 }
+
+export function getStaticPaths() {
+  const { data } = getAllCourses()
+
+  return {
+    paths: data.map(c => ({
+      params: {
+        slug: c.slug
+      }
+    })),
+    fallback: false
+  }
+}
+
+
+export function getStaticProps({params}) {
+  const { data } = getAllCourses()
+  const course = data.filter(c => c.slug === params.slug)[0]
+
+  return {
+    props: {
+      course
+    }
+  }
+}
