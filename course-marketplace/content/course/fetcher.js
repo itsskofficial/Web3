6f6b2ff1a1f26ea891c92d1174c625aa8403825f
SK <sarthakkarandikar03@gmail.com>
2023-04-12 10:05:05 +0000

    Apr 12, 2023, 3:35 PM

--- course-marketplace/content/course/fetcher.js
+++ course-marketplace/content/course/fetcher.js
@@ -2,7 +2,8 @@ const fetchCourseData = () => {
     return (
         data: courses,
         courseMap: courses.reduce((a, c, i) => {
-            
+            a[c.id] = c,
+            a[c.id]
         }, {})
     )
 }
