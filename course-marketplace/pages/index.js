21b09d5e1dff3487c454e14c3a5fadbfeb8ab36d
SK <sarthakkarandikar03@gmail.com>
2023-04-12 10:31:22 +0000

    Apr 12, 2023, 4:01 PM

--- course-marketplace/pages/index.js
+++ course-marketplace/pages/index.js
@@ -6,8 +6,8 @@ export default function Home(props) {
   return (
       <BaseLayout>
         <Hero />
-        {props.courses}
-        <CourseList />
+        
+        <CourseList courses=/>
       </BaseLayout>
   )
 }
