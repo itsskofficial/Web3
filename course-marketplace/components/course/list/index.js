86af23a8d0665c2019157125c5ec454531149977
SK <sarthakkarandikar03@gmail.com>
2023-04-12 15:07:44 +0000

    Apr 12, 2023, 8:37 PM

--- course-marketplace/components/course/list/index.js
+++ course-marketplace/components/course/list/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-
+import Link from "next/link"
 
 export default function List({courses}) {
   return (
@@ -24,11 +24,12 @@ export default function List({courses}) {
                 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
                 {course.type}
               </div>
-              <a
-                href="#"
-                className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-                {course.title}
-              </a>
+              <Link href={`/courses/${course.slug}`}>
+                <a
+                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                  {course.title}
+                </a>
+              </Link>
               <p
                 className="mt-2 text-gray-500">
                 {course.description}
