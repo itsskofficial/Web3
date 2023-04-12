22817921f79f0fdde02bc72e749f71f84c932c1e
SK <sarthakkarandikar03@gmail.com>
2023-04-12 15:18:21 +0000

    Apr 12, 2023, 8:48 PM

--- course-marketplace/pages/courses/[course].js
+++ course-marketplace/pages/courses/[course].js
@@ -4,18 +4,47 @@ import {
   Curriculum,
   Keypoints
 } from "@components/course";
-import BaseLayout from "@components/layout/base";
+import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({course}) {
 
   return (
-    <BaseLayout>
+    <>
+      {course.title}
       <div className="py-4">
-        <CourseHero/>
+        <CourseHero />
       </div>
       <Keypoints />
       <Curriculum />
       <Modal />
-    </BaseLayout>  
+    </>
   )
 }
+
+export function getStaticPaths() {
+  const { data } = getAllCourses()
+
+  return {
+    paths: data.map(c => ({
+      params: {
+        slug: c.slug
+      }
+    })),
+    fallback: false
+  }
+}
+
+
+export function getStaticProps({params}) {
+  const { data } = getAllCourses()
+  const course = data.filter(c => c.slug === params.slug)[0]
+
+  return {
+    props: {
+      course
+    }
+  }
+}
+
+Course.Layout = BaseLayout
