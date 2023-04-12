965f6b569dfe35ed326204a2a49353a098495221
SK <sarthakkarandikar03@gmail.com>
2023-04-12 14:56:12 +0000

    Apr 12, 2023, 8:26 PM

--- course-marketplace/next.config.js
+++ course-marketplace/next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  module.exports = {
     reactStrictMode: true,
     images: {
       domains: [
         "thrangra.sirv.com"
       ]
     }
-  }
 }
 
 module.exports = nextConfig
