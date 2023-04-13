6905775284f5ddf0a49e0025a91788c73b0fd13b
SK <sarthakkarandikar03@gmail.com>
2023-04-13 04:09:19 +0000

    Apr 13, 2023, 9:39 AM

--- course-marketplace/components/common/navbar/index.js
+++ course-marketplace/components/common/navbar/index.js
@@ -43,7 +43,7 @@ export default function Footer() {
               </Link>
               {isLoading ? 
                 <button
-                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                  className=" disabled:px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                     Loading
                 </button> :
                 isWeb3Loaded ?
