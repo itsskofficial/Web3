4f1adcbad18df7040538314add49b8c68b35a56e
SK <sarthakkarandikar03@gmail.com>
2023-04-13 04:07:42 +0000

    Apr 13, 2023, 9:37 AM

--- course-marketplace/components/common/navbar/index.js
+++ course-marketplace/components/common/navbar/index.js
@@ -53,12 +53,11 @@ export default function Footer() {
                     Connect Wallet
                   </button> :
                   <button
-                    onClick={() => {router.push()}}
+                    onClick={() => {router.push('https://metamask.io')}}
                     className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                       Install Metamask
                   </button>
               }
-              
             </div>
           </div>
         </nav>
