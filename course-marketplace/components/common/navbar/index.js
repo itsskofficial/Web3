df0e8d20175288a76d0a52d6aa79213a36f8b651
SK <sarthakkarandikar03@gmail.com>
2023-04-13 05:13:57 +0000

    Apr 13, 2023, 10:43 AM

--- course-marketplace/components/common/navbar/index.js
+++ course-marketplace/components/common/navbar/index.js
@@ -48,6 +48,11 @@ export default function Footer() {
                 </button> :
                 isWeb3Loaded ?
                   account ?
+                  <button
+                  onClick={web3.connect}
+                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                    Connect Wallet
+                  </button> :
                 <button
                   onClick={web3.connect}
                   className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
