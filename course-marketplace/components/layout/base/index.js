221be55a313117d8d099296dcc287cb424105c1d
SK <sarthakkarandikar03@gmail.com>
2023-04-13 00:34:14 +0000

    Apr 13, 2023, 6:04 AM

---
+++ course-marketplace/components/layout/base/index.js
@@ -0,0 +1,20 @@
+import { Footer, Navbar } from "@components/common";
+import { Web3Provider } from "@components/providers";
+import { Fragment } from "react";
+
+export default function BaseLayout(props) {
+    return (
+        <Fragment>
+            <web3
+            <div className="relative max-w-7xl mx-auto px-4">
+                <Navbar />
+                    <div className="fit">
+                        {props.children}
+                    </div>
+                </div>
+            <Footer />
+            <Web3Provider/>
+        </Fragment>
+        
+    )
+}
