930c7b40fcef1c3098fd84f6d14d604227d3ecdb
SK <sarthakkarandikar03@gmail.com>
2023-04-13 10:04:40 +0000

    Apr 13, 2023, 3:34 PM

--- course-marketplace/components/providers/web3/hooks/useNetwork.js
+++ course-marketplace/components/providers/web3/hooks/useNetwork.js
@@ -3,7 +3,9 @@ import useSWR from 'swr'
 const useNetwork = () => {
     const { mutate, ...rest } = useSWR(
         web3 ? 'web3/network' : null,
-        async cons
+        async () => {
+            const netId = 
+        }
     )
 }
 
