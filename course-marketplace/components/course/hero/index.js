ab6c706ca4fa8140f5e0b920bf245908cf147bf0
SK <sarthakkarandikar03@gmail.com>
2023-04-13 00:15:43 +0000

    Apr 13, 2023, 5:45 AM

--- course-marketplace/components/course/hero/index.js
+++ course-marketplace/components/course/hero/index.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Hero({
   title,
   description,
@@ -45,7 +47,7 @@ export default function Hero({
           </div>
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img
+          <Image
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
             src={image}
             alt={title}
