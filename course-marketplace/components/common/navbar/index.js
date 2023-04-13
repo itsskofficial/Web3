0589666fafa820dc0fe7ab96733b02e2424f7555
SK <sarthakkarandikar03@gmail.com>
2023-04-13 03:54:13 +0000

    Apr 13, 2023, 9:24 AM

--- course-marketplace/components/common/navbar/index.js
+++ course-marketplace/components/common/navbar/index.js
@@ -39,6 +39,7 @@ export default function Footer() {
                   Wishlist
                 </a>
               </Link>
+              
               <button
                 onClick={web3.connect}
                 className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
