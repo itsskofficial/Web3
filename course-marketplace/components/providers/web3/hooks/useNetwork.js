165ebe76ec40de5af96a5f6f62b766ab75667480
SK <sarthakkarandikar03@gmail.com>
2023-04-13 10:07:10 +0000

    Apr 13, 2023, 3:37 PM

--- course-marketplace/components/providers/web3/hooks/useNetwork.js
+++ course-marketplace/components/providers/web3/hooks/useNetwork.js
@@ -1,12 +1,18 @@
 import useSWR from 'swr'
 
 const useNetwork = () => {
-    const { mutate, ...rest } = useSWR(
-        web3 ? 'web3/network' : null,
-        async () => {
-            const netId = web3.eth.net.getId()
-            return netId
-        }
+    const { mutate, ...rest } = useSWR(() =>
+    web3 ? "web3/network" : null,
+    async () => {
+      const netId = await web3.eth.net.getId()
+      return netId
+    }
+  )
+
+  useEffect(() => {
+    provider &&
+    provider.on("chainChanged", netId => mutate(netId))
+  }, [web3])
     )
 }
 
