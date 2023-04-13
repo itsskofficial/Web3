202b3d64553e7528c006b50dabcf52e0cec07130
SK <sarthakkarandikar03@gmail.com>
2023-04-13 03:55:06 +0000

    Apr 13, 2023, 9:25 AM

--- course-marketplace/components/common/navbar/index.js
+++ course-marketplace/components/common/navbar/index.js
@@ -41,10 +41,10 @@ export default function Footer() {
               </Link>
               {isLoading ? 
                 <button
-                  onClick={web3.connect}
                   className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                     Loading
-                </button>
+                </button> :
+                
               }
               
             </div>
