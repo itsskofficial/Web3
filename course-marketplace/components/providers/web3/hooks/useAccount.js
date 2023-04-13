1449949e2d336b7a67ace20bdc1fcbd59e2a5b6e
SK <sarthakkarandikar03@gmail.com>
2023-04-13 04:49:14 +0000

    Apr 13, 2023, 10:19 AM

--- course-marketplace/components/providers/web3/hooks/useAccount.js
+++ course-marketplace/components/providers/web3/hooks/useAccount.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react"
 
 const useAccount = (web3) => {
     const [account, setAccount] = useState(null)
-    useEffect(asy() => {
+    useEffect(async () => {
         account = await web3.eth.getAccounts()
+        
     })
     return(
      web3 ? 'Test' : null
