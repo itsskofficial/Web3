acc5b42734004765d0af1854fe4cb16bc65f5562
SK <sarthakkarandikar03@gmail.com>
2023-04-13 01:10:43 +0000

    Apr 13, 2023, 6:40 AM

--- course-marketplace/pages/index.js
+++ course-marketplace/pages/index.js
@@ -1,9 +1,11 @@
 import { CourseList } from "@components/course"
 import BaseLayout from "@components/layout/base"
+import { Web3Provider } from "@components/providers"
 import fetchCourseData from "@content/course/fetcher"
+import { useContext } from "react"
 
 export default function Home(props) {
-  
+  const web3API = useContext(Web3Provider)
   return (
       <BaseLayout>
         <Hero />
