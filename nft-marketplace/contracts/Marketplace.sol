f1b7f48581abbd581760ec5a6afac15b1da735c4
SK <sarthakkarandikar03@gmail.com>
2023-05-03 06:08:53 +0000

    May 3, 2023, 11:38 AM

--- nft-marketplace/contracts/Marketplace.sol
+++ nft-marketplace/contracts/Marketplace.sol
@@ -140,11 +140,10 @@ contract Marketplace is ERC721URIStorage {
 
             NFT[] memory items = new NFT[](itemCount);
             for (uint256 j = 0; j < totalCount; j++) {
-                if (NFTs[j+1].)
-                uint256 currentId = j+1;
-                NFT storage currentItem = NFTs[currentId];
-                items[currentIndex] = currentItem;
-                currentIndex += 1;
+                if (NFTs[j+1].owner == msg.sender){
+
+                }
+                
             }
         }
 
