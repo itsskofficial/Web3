a6e4a45d619dcbc8b2c503d4c389963904e31d6c
SK <sarthakkarandikar03@gmail.com>
2023-05-03 04:23:20 +0000

    May 3, 2023, 9:53 AM

--- nft-marketplace/contracts/Marketplace.sol
+++ nft-marketplace/contracts/Marketplace.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.9;
 
 //internal imports for nft openzeppelin
 import '@openzeppelin/contracts/utils/Counters.sol'; //using as a contract counter
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
