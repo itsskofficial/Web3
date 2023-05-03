36f7aaf6495da03a49a00aa0816ffab395212da3
SK <sarthakkarandikar03@gmail.com>
2023-05-03 04:09:46 +0000

    May 3, 2023, 9:39 AM

--- nft-marketplace/scripts/deploy.js
+++ nft-marketplace/scripts/deploy.js
@@ -1,11 +1,6 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
   const Lock = await hre.ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
