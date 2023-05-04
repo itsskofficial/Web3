93549461abfb4cfd36c8e1cf5a7a634f5daacc06
SK <sarthakkarandikar03@gmail.com>
2023-05-04 04:05:42 +0000

    May 4, 2023, 9:35 AM

--- nft-marketplace/components/Navbar/Navbar.js
+++ nft-marketplace/components/Navbar/Navbar.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { MdNotifications } from 'react-icons/md'
-import {BsSearch} from 'react'
+import { BsSearch } from 'react-icons/bs'
+import {CgMenuLeft}
 
 const Navbar = () => {
     return (
