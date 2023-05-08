e212b889d5bb3cfe244381f709d84e45c496a5a7
SK <sarthakkarandikar03@gmail.com>
2023-05-08 05:42:28 +0000

    May 8, 2023, 11:12 AM

--- nft-marketplace/components/Navbar/Notifications/Notifications.js
+++ nft-marketplace/components/Navbar/Notifications/Notifications.js
@@ -11,7 +11,7 @@ const Notifications = () => {
                 <div className={styles.notificationBoxImg}>
                     <Image src={images.user1} alt='Profile Image' width='50' height='50'/>
                 </div>
-                <div
+                <div className={}
             </div>
         </div>
     )
