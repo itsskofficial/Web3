827cfcbdb89895538cadafc9ea528ad26244011e
SK <sarthakkarandikar03@gmail.com>
2023-05-08 05:31:43 +0000

    May 8, 2023, 11:01 AM

--- nft-marketplace/components/Navbar/Help/Help.js
+++ nft-marketplace/components/Navbar/Help/Help.js
@@ -3,7 +3,16 @@ import styles from './Help.module.css'
 const Help = () => {
     const help = [
         {
-        
+            name: 'Account Settings',
+            link: 'account-settings'
+        },
+        {
+            name: 'Connect Wallet',
+            link: 'connect-wallet'
+        },
+        {
+            name: 'Blog',
+            link: 'blog'
         }
     ]
     return ( 
