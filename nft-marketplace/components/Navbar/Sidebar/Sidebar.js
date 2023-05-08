c2954abdf3328f5228a48e4e2587f75491893320
SK <sarthakkarandikar03@gmail.com>
2023-05-08 06:14:37 +0000

    May 8, 2023, 11:44 AM

--- nft-marketplace/components/Navbar/Sidebar/Sidebar.js
+++ nft-marketplace/components/Navbar/Sidebar/Sidebar.js
@@ -39,6 +39,28 @@ const Sidebar = () => {
             link: 'blog'
         }
     ]
+    const help = [
+        {
+            name: 'About',
+            link: 'about'
+        },
+        {
+            name: 'Contact Us',
+            link: 'contact-us'
+        },
+        {
+            name: 'Signup',
+            link: 'signup'
+        },
+        {
+            name: 'Signin',
+            link: 'signin'
+        },
+        {
+            name: 'Subscription',
+            link: 'subscription'
+        }
+    ]
     return (
 
     )
