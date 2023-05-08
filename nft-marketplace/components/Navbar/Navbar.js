dc7d6a5e5825d6b1c2f2547c50334e1f29b6c26c
SK <sarthakkarandikar03@gmail.com>
2023-05-08 05:10:58 +0000

    May 8, 2023, 10:40 AM

--- nft-marketplace/components/Navbar/Navbar.js
+++ nft-marketplace/components/Navbar/Navbar.js
@@ -106,7 +106,7 @@ const Navbar = () => {
             </div>
             {openSideMenu && (
                 <div className={styles.sidebar}>
-                    <SideBar setOpenSideMenu={setOpenSideMenu} />
+                    <Sidebar setOpenSideMenu={setOpenSideMenu} />
                 </div>
             )}
         </div>
