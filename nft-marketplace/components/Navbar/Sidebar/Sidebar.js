143e64e0ea3c847fdc101610f4ff48b0e0007bdc
SK <sarthakkarandikar03@gmail.com>
2023-05-08 06:12:24 +0000

    May 8, 2023, 11:42 AM

--- nft-marketplace/components/Navbar/Sidebar/Sidebar.js
+++ nft-marketplace/components/Navbar/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import { GrClose } from 'react-icons/gr'
 import { TiSocialFacebook, TdSocialLinkedin, TiSocialInstagram, TiArrowSortedDown, TiSocialYoutube, TiArrowSortedUp } from 'react-icons/ti'
-
+import styles from './Sidebar.module.css'
+import images from '@public/'
 
 const Sidebar = () => {
     return (
