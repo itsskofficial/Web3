4fb12fd74a4e118fed69931271f651922cfac933
SK <sarthakkarandikar03@gmail.com>
2023-05-08 05:15:43 +0000

    May 8, 2023, 10:45 AM

--- nft-marketplace/pages/_app.js
+++ nft-marketplace/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
-
+import Navbar from '@components/Navbar/Navbar'
 const App = ({ Component, pageProps }) => {
     return (
+        <Navbar></Navbar>
         <Component {...pageProps} />
     )
 }
