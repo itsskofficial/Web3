5281afc8b6d875498519ed570fbab431d5bdddf1
SK <sarthakkarandikar03@gmail.com>
2023-05-08 05:29:05 +0000

    May 8, 2023, 10:59 AM

--- nft-marketplace/components/Navbar/Discover/Discover.js
+++ nft-marketplace/components/Navbar/Discover/Discover.js
@@ -36,7 +36,7 @@ const Discover = () => {
         <div>
             {discover.map(i => {
                 <div className={styles.discover} key={i + 1}>
-                    <Link href={{pathname:``}} 
+                    <Link href={{pathname:`${e.link}`}} 
                 </div>
             })}
     )
