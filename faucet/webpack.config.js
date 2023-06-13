a5352acd25180ddcbcb98a4ea7e2b5fd153994ca
SK <sarthakkarandikar03@gmail.com>
2023-06-13 08:44:37 +0000

    Jun 13, 2023, 2:14 PM

--- faucet/webpack.config.js
+++ faucet/webpack.config.js
@@ -11,7 +11,7 @@
                                     "https": false,
                                         "stream": false,
                                             "crypto": false,
-                                                "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+        "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
       }
     },
     entry: [...],
