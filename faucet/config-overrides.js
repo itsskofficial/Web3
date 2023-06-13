574c0acab41fedd8c4bcf9b30b8e3b5a71cde072
SK <sarthakkarandikar03@gmail.com>
2023-06-13 08:52:10 +0000

    Jun 13, 2023, 2:22 PM

--- faucet/config-overrides.js
+++ faucet/config-overrides.js
@@ -10,7 +10,7 @@ module.exports = function override(config) {
         "https": require.resolve("https-browserify"),
         "os": require.resolve("os-browserify"),
         "url": require.resolve("url")
-        "url": require.resolve("url")
+        "path": require.resolve("path")
         
     })
     config.resolve.fallback = fallback;
