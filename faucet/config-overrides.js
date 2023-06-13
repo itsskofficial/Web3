bbb14e745cc107a331527f1915fb2be7cb633039
SK <sarthakkarandikar03@gmail.com>
2023-06-13 08:49:16 +0000

    Jun 13, 2023, 2:19 PM

--- faucet/config-overrides.js
+++ faucet/config-overrides.js
@@ -18,5 +18,7 @@ module.exports = function override(config) {
             Buffer: ['buffer', 'Buffer']
         })
     ])
+    config.ignoreWarnings = [/Failed to parse source map/];
+    
     return config;
 }
