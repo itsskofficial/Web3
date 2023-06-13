204433b00874580d5bb426474b181f6b7050c115
SK <sarthakkarandikar03@gmail.com>
2023-06-13 08:56:34 +0000

    Jun 13, 2023, 2:26 PM

--- faucet/config-overrides.js
+++ faucet/config-overrides.js
@@ -9,7 +9,7 @@ module.exports = function override(config) {
         "http": require.resolve("stream-http"),
         "https": require.resolve("https-browserify"),
         "os": require.resolve("os-browserify"),
-        "url": require.resolve("url")
+        "url": require.resolve("url"),
         "path": require.resolve("path")
         
     })
