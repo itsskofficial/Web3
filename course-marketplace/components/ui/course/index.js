6fd036dd6c085d82b7e80c06353b12d8cde081b8
SK <sarthakkarandikar03@gmail.com>
2023-06-15 11:21:31 +0000

    Jun 15, 2023, 4:51 PM

--- course-marketplace/components/ui/course/index.js
+++ course-marketplace/components/ui/course/index.js
@@ -1,11 +1,11 @@
 
 
 
-
-export default function List({courses, children}) {
-  return (
-    <section className="grid md:grid-cols-1 lg:grid-cols-2 gap-4 mb-5">
-      { courses.map(course => children(course))}
-    </section>
-  )
-}
+export { default as CourseList } from "./list"
+export { default as CourseCard } from "./card"
+export { default as OwnedCourseCard } from "./card/Owned"
+export { default as ManagedCourseCard } from "./card/Managed"
+export { default as CourseHero } from "./hero"
+export { default as Curriculum } from "./curriculum"
+export { default as Keypoints } from "./keypoints"
+export { default as CourseFilter } from "./filter"
