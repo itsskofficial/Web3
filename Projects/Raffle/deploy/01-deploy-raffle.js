a805a969ad4d58b44f482313334ae091ec3ec77b
SK <sarthakkarandikar03@gmail.com>
2024-06-19 14:13:21 +0530

    added mocks

--- Projects/Raffle/deploy/01-deploy-raffle.js
+++ Projects/Raffle/deploy/01-deploy-raffle.js
@@ -1,23 +1,37 @@
-const { getNamedAccounts, deployments, network } = require("hardhat")
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+const VRF_SUBSCRIPTION_FUND_AMOUNT = ethers.parseEther("5")
 
 module.exports = async () => {
     const { deployer } = await getNamedAccounts()
     const { deploy, log } = deployments
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId
+    let vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId, callbackGasLimit, interval
 
     if (developmentChains.includes(network.name)) {
-        const vrfCoordinatorV2Mock = await deployments.get("VRFCoordinatorV2Mock")
+        const vrfCoordinatorV2MockDeployment = await deployments.get("VRFCoordinatorV2Mock")
+        const vrfCoordinatorV2Mock = await ethers.getContractAt(vrfCoordinatorV2MockDeployment.abi, vrfCoordinatorV2MockDeployment.address)
+        let transactionResponse, transactionReceipt
 
-        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.target
+        transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        transactionReceipt = await transactionResponse.wait()
+        subscriptionId = BigInt(transactionReceipt.logs[0].topics[1]);
+
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUBSCRIPTION_FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     entranceFee = networkConfig[chainId]["entranceFee"]
     gasLane = networkConfig[chainId]["gasLane"]
-    subscriptionId = networkConfig[chainId]["subscriptionId"]
+    callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    interval = networkConfig[chainId]["interval"]
+
+    log("Deploying Raffle...")
 
     const raffle = await deploy("Raffle", {
         from: deployer,
@@ -25,9 +39,28 @@ module.exports = async () => {
             vrfCoordinatorV2Address,
             entranceFee,
             gasLane,
-            subscriptionId
+            subscriptionId,
+            callbackGasLimit,
+            interval
         ],
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1
     })
+
+    log("Raffle deployed!")
+    log("-------------------")
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, [
+            vrfCoordinatorV2Address,
+            entranceFee,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit,
+            interval
+        ])
+    }
 }
+
+module.exports.tags = ["all", "raffle"]
--- Projects/Raffle/helper-hardhat-config.js
+++ Projects/Raffle/helper-hardhat-config.js
@@ -9,6 +9,7 @@ const networkConfig = {
             "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae",
         subscriptionId: "0",
         callbackGasLimit: "500000",
+        interval: "30",
     },
     31337: {
         name: "localhost",
@@ -16,6 +17,7 @@ const networkConfig = {
         gasLane:
             "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae",
         callbackGasLimit: "500000",
+        interval: "30",
     },
 };
 
