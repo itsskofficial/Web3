5e105fa91a5d6e63fed96d5e282bc2ecf0b4b2d9
SK <sarthakkarandikar03@gmail.com>
2024-07-13 07:20:08 +0530

    progressed on frontend for tokenswap

--- Projects/TokenSwap/frontend/components/SwapComponent.js
+++ Projects/TokenSwap/frontend/components/SwapComponent.js
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+
+import { checkAllowance, increaseAllowance, swapEthToToken, swapTokenToToken, swapTokenToEth } from "@utils/context";
+import { CogIcon, ArrowSmDownIcon } from "@heroicons/react/outline";
+import SwapField from "./SwapField";
+import TransactionStatus from "./TransactionStatus";
+import toast, { Toaster } from "react-hot-toast";
+import { weiToEth, ethToWei } from "@utils/helpers";
+import { TOKENS } from "@utils/tokens";
 
 const SwapComponent = () => {
-  return <div>SwapComponent</div>;
+  const [tokenA, setTokenA] = useState(TOKENS["ETH"])
+  const [tokenB, setTokenB] = useState(TOKENS["USDT"])
+  const [inputValue, setInputValue] = useState(0)
+  const [outputValue, setOutputValue] = useState(0)
+
+  const inputValueRef = useRef()
+  const outputValueRef = useRef()
+  const isReversed = useRef(false)
+  const [allowanceStatus, setAllowanceStatus] = useState("Increase allowance")
+  const [amountStatus, setAmountStatus] = useState("Enter amount")
+  const [walletStatus, setWalletStatus] = useState("Connect wallet")
+  const [swapStatus, setSwapStatus] = useState("Swap")
+
+  const tokenAObj = {
+    id: "tokenA",
+    value: inputValue,
+    setValue: setInputValue,
+    defaultValue: tokenA,
+    ignoreValue: tokenB,
+    setToken: setTokenA
+  }
+
+  const tokenBObj = {
+    id: "tokenB",
+    value: outputValue,
+    setValue: setOutputValue,
+    defaultValue: tokenB,
+    ignoreValue: tokenA,
+    setToken: setTokenB
+  }
 };
 
 export default SwapComponent;
